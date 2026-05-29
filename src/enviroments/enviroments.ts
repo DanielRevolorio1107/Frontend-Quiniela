007715dedcbb80f94dff45d163f4c8aacc4adba6
rramirezg18 <rramirezg18@miumg.edu.gt>
2026-05-28 20:51:28 -0600

    fix url backend

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://quiniela-production-62e5.up.railway.app/api/v1',
-    hubUrl: 'https://quiniela-production-62e5.up.railway.app/hubs/quiniela'
+    apiUrl: 'https://quiniela-production-62e5.up.railway.app/api/v1'
 };
